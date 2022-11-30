66421bbd3f71c05c9ce2cc456e459ffcf442d252
Mattia Capelli <mattia@mattiacapelli.it>
2022-11-30 10:13:18 +0100

    Building the auth function

--- src/client/client-smanager/client-smanager/Form1.Designer.cs
+++ src/client/client-smanager/client-smanager/Form1.Designer.cs
@@ -28,21 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_action = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_auth = new System.Windows.Forms.Label();
+            this.lbl_connection = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btn_action
+            // 
+            this.btn_action.Location = new System.Drawing.Point(12, 100);
+            this.btn_action.Name = "btn_action";
+            this.btn_action.Size = new System.Drawing.Size(253, 59);
+            this.btn_action.TabIndex = 0;
+            this.btn_action.Text = "btn_action";
+            this.btn_action.UseVisualStyleBackColor = true;
+            this.btn_action.Click += new System.EventHandler(this.btn_action_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<>";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbl_auth
+            // 
+            this.lbl_auth.AutoSize = true;
+            this.lbl_auth.Location = new System.Drawing.Point(12, 173);
+            this.lbl_auth.Name = "lbl_auth";
+            this.lbl_auth.Size = new System.Drawing.Size(44, 13);
+            this.lbl_auth.TabIndex = 2;
+            this.lbl_auth.Text = "lbl_auth";
+            // 
+            // lbl_connection
+            // 
+            this.lbl_connection.AutoSize = true;
+            this.lbl_connection.Location = new System.Drawing.Point(12, 193);
+            this.lbl_connection.Name = "lbl_connection";
+            this.lbl_connection.Size = new System.Drawing.Size(76, 13);
+            this.lbl_connection.TabIndex = 3;
+            this.lbl_connection.Text = "lbl_connection";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(277, 215);
+            this.Controls.Add(this.lbl_connection);
+            this.Controls.Add(this.lbl_auth);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_action);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_action;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_auth;
+        private System.Windows.Forms.Label lbl_connection;
     }
 }
 
--- src/client/client-smanager/client-smanager/Form1.cs
+++ src/client/client-smanager/client-smanager/Form1.cs
@@ -1,20 +1,92 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace client_smanager
 {
     public partial class Form1 : Form
     {
+        string keypath = "./auth.key";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public bool authenticate(string key) 
+        {
+            
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(keypath))
+            {
+                //Change the btn_action Text and update the lbl_auth text and color
+                btn_action.Text = "Authenticate";
+                lbl_auth.Text = "Not Authenticated";
+                lbl_auth.ForeColor = Color.Red;
+            }
+        }
+
+        private void btn_action_Click(object sender, EventArgs e)
+        {
+            switch (btn_action.Text)
+            {
+                case "Authenticate":
+
+                    //Try to authenticate to the server
+                    if (authenticate())
+                    {
+                        lbl_auth.Text = "Authenticated";
+                        lbl_auth.ForeColor = Color.Green;
+                        btn_action.Text = "Start";
+                    }
+                    else
+                    {
+                        lbl_auth.Text = "Wrong Key";
+                        lbl_auth.ForeColor = Color.Red;
+                        btn_action.Enabled = false;
+                        Thread.Sleep(5000);
+                        btn_action.Enabled = true;
+                        lbl_auth.Text = "Not Authenticated";
+                        lbl_auth.ForeColor = Color.Red;
+                    }
+
+                    break;
+
+                case "Start":
+
+                    break;
+
+                case "Stop":
+
+                    break;
+            }
         }
     }
 }
